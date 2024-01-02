6f2be1d266c78d207d403b63ec1ea2116ad70168
Manuel Alejandro <alecuba96@gmail.com>
2024-01-02 21:53:43 +0100

    Fixed type error

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -1,11 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import type { AstroI18nextConfig } from "astro-i18next";
 
 const config: AstroI18nextConfig = {
-    defaultLocale: "es",
-    locales: ["es", "en"],
-    i18nextServer: {
-        debug: true,
-    },
+  defaultLocale: "es",
+  locales: ["es", "en"],
+  i18nextServer: {
+    debug: true,
+  },
 };
 
 export default config;
